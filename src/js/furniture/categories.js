6256943e7df782325479e2de14a27345ccdaeb2a
Olexandr Usarov <skeler228@gmail.com>
2026-04-04 19:35:08 +0300

    first commit

--- src/js/furniture/categories.js
+++ src/js/furniture/categories.js
@@ -1,3 +1,6 @@
+import { getCategories } from '../api/furniture-api';
+import { refs } from '../helpers/refs';
+
 export function initCategories() {
   document.addEventListener('click', (event) => {
     const clickedButton = event.target.closest('[data-category-button]');
@@ -14,4 +17,13 @@ export function initCategories() {
 
     clickedButton.classList.add('is-active');
   });
+}
+console.log(getCategories());
+function renderCategories(array) {
+  const markup = array
+    .map((category) => {
+      return `<button type="button" class="category-btn" data-category-button data-categoryId="${category.id}>${category.name}</button>`;
+    })
+    .join('');
+  refs.categoriesList.innerHTML = markup;
 }
